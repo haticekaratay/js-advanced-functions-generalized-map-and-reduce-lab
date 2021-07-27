334fa9ad5a6d7e8a257c5abed4a4e7d0d2ea9511
Hatice <haticekaraty@gmail.com>
2021-07-26 22:31:37 -0400

    commit tree 8df5a1ef6d720882a153fb9bba5e9ca1d1ff7935

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your functions here
+function map(arr,f){
+    let newArray = [];
+    for(let i = 0; i < arr.length; i++){
+        newArray.push(f(arr[i]));
+    }
+    return newArray;
+};
+
+function reduce(arr,f,startingPoint){
+    let total = 0;
+    if(startingPoint){
+        total = startingPoint;
+        for(let i = 0; i < arr.length; i++){
+            total = f(arr[i],total)
+        }
+    }else{
+        total =arr[0];
+        for(let i = 1; i < arr.length; i++){
+            total = f(arr[i],total)
+        }
+    }
+    return total;
+};
+//console.log(reduce([1,2,3],function(e, memo){return e + memo}, 100))
